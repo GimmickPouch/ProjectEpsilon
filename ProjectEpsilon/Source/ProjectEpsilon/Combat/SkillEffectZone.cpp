721a691230f133c7f0849e20487669c2a6db8d4e
DESKTOP-BHO2964\eslpd <luke.alexander.perry@gmail.com>
2018-12-11 21:56:43 +0000

    Added class for skill zone

---
+++ ProjectEpsilon/Source/ProjectEpsilon/Combat/SkillEffectZone.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SkillEffectZone.h"
+
+
+// Sets default values
+ASkillEffectZone::ASkillEffectZone()
+{
+    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASkillEffectZone::BeginPlay()
+{
+    Super::BeginPlay();
+    
+}
+
+// Called every frame
+void ASkillEffectZone::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+}
+
